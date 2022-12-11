6197b5c45892f7e11b85c46fbec85a526ed9667f
Liam Hess <liam.m.hess@gmail.com>
2022-12-11 23:23:46 +0100

    return of full tracklist of playlist

--- api_calls.py
+++ api_calls.py
@@ -1,4 +1,5 @@
 from internet import *
+import math
 
 
 class ApiCalls:
@@ -46,12 +47,37 @@ class ApiCalls:
             "Content-Type": "application/json"
         }
         params = {
-            "limit": 1,
-            "fields": "items(track(name), added_at)"
+            "limit": 50,
+            "fields": "total, next, items(track(name), added_at)"
         }
 
         r = requests.get(url=url, headers=headers, params=params)
         playlist_obj = r.json()
-        return playlist_obj
+        
 
-# WARUM IST DIE ITEMS LIST IN DER RESPONES LEER????
+        # Liste mit allen Songs der Playlist erstellen
+        song_list = []
+        needed_request_amount = math.ceil(playlist_obj["total"] / 50)
+        # äußere Schleife macht requests und erhält 50er liste an songs,
+        # die dann zu neuem einfacherem dict processed werden und in song_list geaddet werden
+        # nach jedem Durchlauf, wird current_part_playlist_obj auf den nächsten Teilabschnitt aktualisiert
+        # counter = 1
+        current_part_playlist_obj = playlist_obj
+        for i in range(0, needed_request_amount):
+            current_items = current_part_playlist_obj["items"]
+            for song in current_items:
+                song_dict = {
+                    "name": song["track"]["name"],
+                    "added_at": song["added_at"]
+                }
+                song_list.append(song_dict)
+                # print(str(counter) + ". " + song_dict["name"] + " hinzugeüft am: " + song_dict["added_at"])
+                # counter += 1
+            
+            # nur wenn es nicht die letzte request ist, wird das current_part_playlist_obj geupdatet
+            if i != needed_request_amount-1:
+                next_url = current_part_playlist_obj["next"]
+                current_part_playlist_obj = self.basic_api_call(next_url)
+    
+
+        return song_list
