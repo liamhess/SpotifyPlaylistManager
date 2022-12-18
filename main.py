f5c1c46e7be6964be6926c05db18752ec033d1ed
Liam Hess <liam.m.hess@gmail.com>
2022-12-18 23:05:39 +0100

    main function

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from internet import *
 from api_calls import *
 from logic import *
 
-i = Internet()
-a = ApiCalls(i.give_valid_access_token())
-l = Logic()
+
 
 # test playlist id = 60QjB7v7V8If58q661ZzOj
 # echte drip drop playlist id = 6NYUUALff1pcZRJHaLpvRA
@@ -21,24 +19,30 @@ Reihenfolge:
 7. Songs aus dripdrop removen
 """
 
+def main():
 
-# 1. Playlists vergleichen
-songs_to_add = l.playlist_comparison(realplaylist="6NYUUALff1pcZRJHaLpvRA", backupplaylist="7kEpthO12IIpyOSuXnxPXD")
+    # 0. Initialisierung der Klassen
+    i = Internet()
+    a = ApiCalls(i.give_valid_access_token())
+    l = Logic()
 
-# 2. Fehlende Songs zu backup Playlist adden
-a.add_tracks(songs_to_add)
+    # 1. Playlists vergleichen
+    songs_to_add = l.playlist_comparison(realplaylist="6NYUUALff1pcZRJHaLpvRA", backupplaylist="7kEpthO12IIpyOSuXnxPXD")
 
-# 3. song_dict mit dripdrop Playlist erstellen
-song_dict = a.get_playlist_items("6NYUUALff1pcZRJHaLpvRA")
+    # 2. Fehlende Songs zu backup Playlist adden
+    a.add_tracks(songs_to_add)
 
-# 4. song_dict runterkürzen auf Songs die älter als 91 Tage sind
-song_dict = l.time_dict_shortener(song_dict)
+    # 3. song_dict mit dripdrop Playlist erstellen
+    song_dict = a.get_playlist_items("6NYUUALff1pcZRJHaLpvRA")
 
-# 5. favorite tag dem song_dict hinzufügen
-l.favorite_dict_adder(song_dict)
+    # 4. song_dict runterkürzen auf Songs die älter als 91 Tage sind
+    song_dict = l.time_dict_shortener(song_dict)
 
-# 6. song_dict kürzen, sodass nur unfavorites drin sind
-song_dict = l.unfavorite_shortener(song_dict)
+    # 5. favorite tag dem song_dict hinzufügen
+    l.favorite_dict_adder(song_dict)
 
-# 7. Songs aus dripdrop entfernen
-a.remove_songs_from_playlist(song_dict)
+    # 6. song_dict kürzen, sodass nur unfavorites drin sind
+    song_dict = l.unfavorite_shortener(song_dict)
+
+    # 7. Songs aus dripdrop entfernen
+    a.remove_songs_from_playlist(song_dict)
